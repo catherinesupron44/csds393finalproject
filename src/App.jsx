05e143f75c3eebac17ba0383e8b670bc978a1fea
berkoved <90010584+berkoved@users.noreply.github.com>
2024-10-16 13:13:48 -0400

    Updated React App setup
    Removed next.js
    Setup App.jsx
    Setup build files
    Updated packages

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
+
+function App() {
+
+    return {
+
+    }
+}
+
+export default App;
