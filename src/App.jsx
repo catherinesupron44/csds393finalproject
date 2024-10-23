a2d74ca80816f06ecb4097a8a79b5a0d53853fe3
berkoved <90010584+berkoved@users.noreply.github.com>
2024-10-23 19:01:19 -0400

    Login and Sign-Up Pages
    Update your local environment:
    1. npm install
    2. amplify pull --appId d1lx3x1gdxpv17 --envName dev

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
+import ProtectedRoutes from "./utils/ProtectedRoutes";
 
 
 import HomePage from './components/homepage';
 import FriendsPage from './components/friends';
+import SignUp from "./components/SignUp";
+import Login from "./components/Login";
 
 function App() {
 
@@ -12,8 +15,13 @@ function App() {
         <Router>
             <AnimatePresence>
                 <Routes>
-                <Route path="" element={<HomePage />} />
-                <Route path="/friends" element={<FriendsPage />} />
+                <Route path="/signup" element={<SignUp />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/" element={<SignUp />} />
+                <Route element={<ProtectedRoutes/>} >
+                    <Route path="/home" element={<HomePage />} />
+                    <Route path="/friends" element={<FriendsPage />} />
+                </Route>
                 </Routes>
             </AnimatePresence>
         </Router>
